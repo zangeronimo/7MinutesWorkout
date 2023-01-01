2b250e15546bcc51b959c8adb8b83d72fd3391a6
Zangeronimo <zangeronimo@gmail.com>
2023-01-01 06:54:02 -0300

    First commit

---
+++ app/src/main/java/br/com/tudolinux/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,27 @@
+package br.com.tudolinux.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import br.com.tudolinux.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this, "Start button clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
