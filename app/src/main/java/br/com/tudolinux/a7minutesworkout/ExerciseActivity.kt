da1d8512fd62ac0a1b3dcf6a7d0cb0f7bb0e3f0d
Zangeronimo <zangeronimo@gmail.com>
2023-01-02 10:36:49 -0300

    feat: use countdowntimer

--- app/src/main/java/br/com/tudolinux/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/br/com/tudolinux/a7minutesworkout/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package br.com.tudolinux.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import br.com.tudolinux.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding : ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,41 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setRestProgressBar(){
+        restTimer = object: CountDownTimer((restProgress * 1000).toLong(), 1000){
+            override fun onTick(p0: Long) {
+                restProgress--
+                binding?.progressBar?.progress = restProgress
+                binding?.tvTimer?.text = "$restProgress"
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"timer finished", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 10
+        }
+
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 10
+        }
+
+        binding = null
     }
 }
